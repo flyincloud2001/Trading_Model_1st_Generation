76392ba5108d330290ed6c857720a37c0ca5a48a
flyincloud2001 <flyincloud2001@gmail.com>
2026-07-31 17:03:13 +0800

    Auto sync: 2026-07-31 17:03:13

--- strategy/cointegration.py
+++ strategy/cointegration.py
@@ -37,7 +37,7 @@ def calc_halflife(spread: pd.Series) -> float:
     delta_spread = delta_spread.dropna()
 
     # OLS 回歸：delta_spread = lambda * spread_lag + mu
-    # mu is the average of  
+    # mu is the mean of spread in the long term
     X = add_constant(spread_lag)
     result = OLS(delta_spread, X).fit()
     lambda_ = result.params.iloc[1]
