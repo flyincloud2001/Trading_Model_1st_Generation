e338bb0db16a7b5b1ea1fe69acd28dab18c61ada
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-01 14:37:01 +0800

    Auto sync: 2026-08-01 14:37:01

--- data/test for fetcher.py
+++ data/test for fetcher.py
@@ -26,8 +26,7 @@ def fetch_single(symbol: str,
     raw = yf.download(symbol, start=start_date, end=end_date, auto_adjust=True, progress=False)
 
     df = raw[['Open', 'Close', 'High', 'Low', 'Volume']]
-    print(len(raw.columns))
-
+    raw.reset_index(drop=True)
     print(raw)
 fetch_single("GLD")
     
