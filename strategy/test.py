d013f9af92537c7779b2b81ca1328b98b89ca5a8
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-06 20:53:28 +0800

    Auto sync: 2026-08-06 20:53:28

--- strategy/test.py
+++ strategy/test.py
@@ -6,6 +6,14 @@ from data.loader import load_multiple
 import matplotlib.pyplot as plt
 import numpy as np
 
-gld, gdx = load_multiple(["GLD", "GDX"])
-print(gld)  
+data = load_multiple(["GLD", "GDX"])
+
+gld = data['GLD']['Close']
+gdx = data['GDX']['Close']
+
+spread = gld - gdx
+
+fig, ax = plt.subplot(figsize=(12, 5))
+ax.plot(spread)
+plt.show()
 
