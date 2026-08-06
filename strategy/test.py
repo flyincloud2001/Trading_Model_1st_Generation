4d35b931375885361ec1ee0772002613f1312c5d
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-06 21:02:56 +0800

    Auto sync: 2026-08-06 21:02:56

--- strategy/test.py
+++ strategy/test.py
@@ -4,7 +4,8 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from data.loader import load_multiple
 from strategy.cointegration import cadf_test
-from strategy.hedge_ratio import 
+from strategy.hedge_ratio import calc_rolling_hedge_ratio
+from strategy.hedge_ratio import calc_spread
 import matplotlib.pyplot as plt
 import numpy as np
 
