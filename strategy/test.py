03d4a46a8a460691fad0af4d2c3a98059a3cd115
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-06 20:51:12 +0800

    Auto sync: 2026-08-06 20:51:12

--- strategy/test.py
+++ strategy/test.py
@@ -1,11 +1,11 @@
 
 import sys
 import os 
-os.path.append(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from data.loader import load_multiple
 import matplotlib.pyplot as plt
 import numpy as np
 
 gld, gdx = load_multiple(["GLD", "GDX"])
-print(gld)
+print(gld)  
 
