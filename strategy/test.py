8c12241377b3475e5b8ebd044f3d5bdfd6f297ea
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-06 20:49:12 +0800

    Auto sync: 2026-08-06 20:49:12

--- strategy/test.py
+++ strategy/test.py
@@ -1,4 +1,6 @@
 import sys
 import os 
 os.path.append(os.path.join(os.path.dirname(__file__), ".."))
-import data.loader 
+from data.loader import load_multiple
+
+gld, gdx = load_multiple(["GLD", "GDX"])
