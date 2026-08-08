a300d2f3131f2be13569e1c747a7cd2f15832a7e
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-08 16:48:05 +0800

    Auto sync: 2026-08-08 16:48:05

--- strategy/test for signal.py
+++ strategy/test for signal.py
@@ -108,6 +108,9 @@ if __name__ == "__main__":
     gld = data['GLD']['Close']
     gdx = data['GDX']['Close']
 
+    gld =  gld[gld.index >= gld.index.max() - pd.DateOffset(months=9)]
+    gdx =  gdx[gdx.index >= gdx.index.max() - pd.DateOffset(months=9)]
+
     # spread 直接取 cadf_test 的 OLS 殘差（均值為 0）
     cadf_result = cadf_test(gld, gdx)
     spread = cadf_result['spread']
