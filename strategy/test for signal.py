118181faac1b26147ae69effe8b30687df67fee7
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-08 16:50:49 +0800

    Auto sync: 2026-08-08 16:50:49

--- strategy/test for signal.py
+++ strategy/test for signal.py
@@ -115,7 +115,6 @@ if __name__ == "__main__":
     cadf_result = cadf_test(gld, gdx)
     spread = cadf_result['spread']
     lookback = cadf_result['half_life']
-    print(lookback)
 
     # 計算 Z-score 與信號
     zscore = calc_zscore(spread, int(lookback))
