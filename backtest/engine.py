f85039f6a86d2958073744b9c7e6103a4e7efe58
flyincloud2001 <flyincloud2001@gmail.com>
2026-08-12 02:44:33 +0800

    Auto sync: 2026-08-12 02:44:33

--- backtest/engine.py
+++ backtest/engine.py
@@ -59,7 +59,7 @@ def run_backtest(series_y: pd.Series,
     # signal = +1：做多 Y，做空 X
     # signal = -1：做空 Y，做多 X
     df['pnl_y'] = df['signal_prev'] * df['return_y']
-    df['pnl_x'] = -df['signal_prev'] * hedge_ratio* df['return_x']
+    df['pnl_x'] = -df['signal_prev'] * hedge_ratio * df['return_x']
     df['pnl_gross'] = (df['pnl_y'] + df['pnl_x']) / 2
     
     # 計算交易成本（每次倉位改變時收取）
